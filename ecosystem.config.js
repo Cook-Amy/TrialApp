3f43184eb1762afaa8f2e311e853cacb46108f5f
Cook-Amy <coo17045@byui.edu>
2020-01-22 17:03:42 -0500

    all changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'http://ec2-18-225-5-189.us-east-2.compute.amazonaws.com/',
-      key: './trialApp.pem',
+      host: 'ec2-18-225-5-189.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/trialApp.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/Cook-Amy/TrialApp.git',
-      path: '/home/ubuntu/trialapp',
+      repo: 'git@github.com:Cook-Amy/TrialApp.git',
+      path: '/home/ubuntu/server',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
