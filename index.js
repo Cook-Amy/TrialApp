bbdf64667cf5fa99ebb9c3c7077dec73fa9be9bf
Cook-Amy <coo17045@byui.edu>
2020-01-24 09:52:57 -0500

    user and password

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const bodyParser = require('body-parser');
 
 const countries = require('./models/countries');
 
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOSTNAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: 'countries'
+// });
+
 const connection = mysql.createConnection({
   host: 'database-1.cc9faes6cfe1.us-east-2.rds.amazonaws.com',
   user: 'admin',
-  password: '',
+  password: 'db2020drapp',
   database: 'countries'
 });
 
